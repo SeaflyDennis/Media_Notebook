192fb01e54ccd301b88f7ab0c52b9f5feb9bc087
SeaflyDennis <seafly0616@qq.com>
2017-07-05 12:29:13 +0800

    添加对任意工具的支持

--- search.sh
+++ search.sh
@@ -26,8 +26,7 @@ function get_search_pure_list()
 {
     # 获取精确纯文件名列表并写入临时文件
     cat ~/seafly_search.temp |\
-        xargs -i -d '\n' echo {} |\
-        awk -F ':' '{print $2}' |\
+        xargs -i -d '\n' echo {} | awk -F ':' '{print $2}' |\
         xargs -i -d '\n' echo {} > $HOME/seafly_search_pure.temp
     echo "~/seafly_search_pure.temp:" ; cat ~/seafly_search_pure.temp
 }
@@ -82,6 +81,16 @@ function get_search_png_list()
               |*.PNG|*.JPG|*.JPEG|*.BMP" > $HOME/seafly_search_media.temp
 }
 
+function get_search_text_list()
+{
+    # 判断过滤出文本文件并将新列表写入临时文件
+    cat ~/seafly_search_pure.temp |\
+        awk -F '\n' '{print $1}' |\
+        egrep "*.txt|*.TXT|*.md|*.markdown|*.MARKDOWN|\
+        *.c|*.cpp|*.s|*.S|*.lds|*Makefile*|*makefile*|\
+        *config*|*build*|*README*|*readme*" > $HOME/seafly_search_media.temp
+}
+
 function get_search_all_path()
 {
     # 获取搜到的所有类型文件路径
@@ -106,6 +115,15 @@ function get_search_final_path()
 
 function check_player()
 {
+    for MEDIA_PLAYER in $PLAYER
+    do
+        which $MEDIA_PLAYER 2>/dev/null 1>/dev/null
+        if [ $? -eq 0 ] ;
+        then
+            PLAYER=$MEDIA_PLAYER
+            break
+        fi
+    done
     which $PLAYER 2>/dev/null 1>/dev/null
     if [ $? -ne 0 ] ;
     then
@@ -160,9 +178,9 @@ fi
 if [ $# -eq 2 ] ;
 then
     case "$1" in
-        "mplayer"|"/usr/bin/mplayer")
+        "mplayer"|"/usr/bin/mplayer")   # gedit
             PLAYER=mplayer
-            INPUT_STRING=$2
+            INPUT_STRING="$2"
             check_player
             get_search_grep_list
             get_search_pure_list
@@ -173,22 +191,22 @@ then
             play_search_list
             clear_temp_file
             ;;
-        "smplayer"|"/usr/bin/smplayer") # media
-            PLAYER=smplayer
-            INPUT_STRING=$2
+        "okular"|"/usr/bin/okular")     # PDF
+            PLAYER="okular"
+            INPUT_STRING="$2"
             check_player
             get_search_grep_list
             get_search_pure_list
-            get_search_media_list
+            get_search_pdf_list
             get_search_all_path
             get_search_final_path
             check_player
             play_search_list
             clear_temp_file
             ;;
-        "okular"|"/usr/bin/okular"|"PDF"|"pdf")     # PDF
-            PLAYER=okular
-            INPUT_STRING=$2
+            "PDF"|"pdf")     # PDF
+            PLAYER="FoxitReader okular"
+            INPUT_STRING="$2"
             check_player
             get_search_grep_list
             get_search_pure_list
@@ -201,7 +219,7 @@ then
             ;;
         "wps"|"/usr/bin/wps"|"DOC"|"doc"|"DOCX"|"docx")           # DOC
             PLAYER=wps
-            INPUT_STRING=$2
+            INPUT_STRING="$2"
             check_player
             get_search_grep_list
             get_search_pure_list
@@ -214,7 +232,7 @@ then
             ;;
         "wpp"|"/usr/bin/wpp"|"PPT"|"ppt"|"PPTX"|"pptx")           # PPT
             PLAYER=wpp
-            INPUT_STRING=$2
+            INPUT_STRING="$2"
             check_player
             get_search_grep_list
             get_search_pure_list
@@ -227,7 +245,7 @@ then
             ;;
         "et"|"/usr/bin/et"|"XLS"|"xls"|"XLSX"|"xlsx")             # xls   (sheet)
             PLAYER=et
-            INPUT_STRING=$2
+            INPUT_STRING="$2"
             check_player
             get_search_grep_list
             get_search_pure_list
@@ -239,8 +257,8 @@ then
             clear_temp_file
             ;;
         "eog"|"/usr/bin/eog"|"PNG"|"png"|"JPG"|"jpg"|"JPEG"|"jpeg"|"BMP"|"bmp"|"IMAGE"|"image")           # Images
-            PLAYER=okular
-            INPUT_STRING=$2
+            PLAYER="eog"
+            INPUT_STRING="$2"
             check_player
             get_search_grep_list
             get_search_pure_list
@@ -251,20 +269,49 @@ then
             play_search_list
             clear_temp_file
             ;;
-        "kazam|KAZAM")
+        "text"|"txt"|"md"|"markdown"|"c"|"cpp"|"s"|\
+        "TEXT"|"TXT"|"MD"|"Markdown"|"C"|"CPP"|"S"|\
+        "lds"|"config"|"make"|"makefile"|"MAKE"|"Makefile"|"MAKEFILE"|\
+        "build"|"readme"|"README")
+            PLAYER="vim gedit emacs"
+            INPUT_STRING="$2"
+            check_player
+            get_search_grep_list
+            get_search_pure_list
+            get_search_text_list
+            get_search_all_path
+            get_search_final_path
+            check_player
+            play_search_list
+            clear_temp_file
+            ;;
+        "marxico"|"马克飞象")
+            echo "一款Chrome下不错的mardown编辑器插件"
+            ;;
+        "kazam"|"KAZAM")
             echo "一款Linux下不错的录屏软件"
             ;;
-        "supertuxkart|SuperTuxKart")
+        "supertuxkart"|"SuperTuxKart")
             echo "一款Linux下不错的跑跑卡丁车游戏"
             ;;
         *)
-            echo "Usage: media.sh [player] keyword"
-            exit 10
+            PLAYER="$1"
+            INPUT_STRING="$2"
+            check_player
+            get_search_grep_list
+            get_search_pure_list
+            #get_search_text_list
+            get_search_all_path
+            get_search_final_path
+            check_player
+            play_search_list
+            clear_temp_file
             ;;
     esac
 elif [ $# -eq 1 ] ;
 then
-    INPUT_STRING=$1
+    PLAYER="smplayer mplayer"
+    INPUT_STRING="$1"
     check_player
     get_search_grep_list
     get_search_pure_list
